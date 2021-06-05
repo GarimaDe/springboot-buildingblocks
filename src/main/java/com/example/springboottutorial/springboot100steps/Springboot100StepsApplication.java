47dd4736bf3a35923c9f226f324533a054f6db27
unknown <garimade3@gmail.com>
2021-06-05 18:53:42 +0530

    First commit

---
+++ src/main/java/com/example/springboottutorial/springboot100steps/Springboot100StepsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottutorial.springboot100steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot100StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot100StepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboottutorial/springboot100steps/Springboot100StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottutorial.springboot100steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot100StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
