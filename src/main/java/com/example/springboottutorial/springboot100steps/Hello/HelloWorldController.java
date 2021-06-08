38f536d5255b89b3547e5142bee85e14c0b11ee7
GarimaDe <garimade3@gmail.com>
2021-06-08 22:30:10 +0530

    Internationalization Implemented

--- src/main/java/com/example/springboottutorial/springboot100steps/Hello/HelloWorldController.java
+++ src/main/java/com/example/springboottutorial/springboot100steps/Hello/HelloWorldController.java
@@ -1,14 +1,18 @@
 package com.example.springboottutorial.springboot100steps.Hello;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 //Controller
 @RestController
 public class HelloWorldController {
 
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 	//Simple Method
 	//URL - /helloworld - Get method
 //	@RequestMapping(method=RequestMethod.GET, path="/helloworld")
@@ -22,5 +26,14 @@ public class HelloWorldController {
 	public UserDetails helloWorldBean()
 	{
 		return  new UserDetails("Garima","De","Kolkata");
+	}
+@GetMapping("/hello-int")
+	public String getMessagesInI18NFormat(@RequestHeader(name="Accept-Language", required = false) String locale){
+		return messageSource.getMessage("label.hello", null, new Locale(locale));
+	}
+
+	@GetMapping("/hello-int2")
+	public String getMessagesInI18NFormat2(){
+		return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
 	}
 }
